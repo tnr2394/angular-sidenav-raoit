e7cf4a105c9f1222278e9dcae1453921030493c7
tnr2394 <tirthrajbarot2394@gmail.com>
2017-06-06 21:38:55 +0530

    Template URL Support added

--- side-nav-directive-raoit.js
+++ side-nav-directive-raoit.js
@@ -1,22 +1,24 @@
 var app = angular.module('angularSideNavModule',[]);
-	app.controller('myCtrl',function($scope){
-		$scope.data = "tirthraj barot";
-	})
-	app.directive('angularSideNavRaoit', function() {
-		return {
-			restrict : 'E',
-			templateUrl : "side-nav-template.html",
-			scope : {},
-			controller : function($scope){
-				$(document).ready(function(){
-					$("#angular-side-nav-open").click(function(){
-						$(".custom-sidenav-raoit").fadeIn();
-						$(".custom-sidenav-raoit ul").animate({'width':'300px'},"slow");
-					});
-					$(".custom-sidenav-raoit").click(function(){
-						$(".custom-sidenav-raoit ul").animate({'width':'0px'},"fast","",function(){$(".custom-sidenav-raoit").fadeOut("fast")});
-					})
+app.controller('myCtrl',function($scope){
+	$scope.data = "tirthraj barot";
+})
+app.directive('angularSideNavRaoit', function() {
+	return {
+		restrict : 'E',
+		templateUrl : function(elem,attrs) {
+			return attrs.templateUrl || "side-nav-template.html"
+		},
+		scope : {},
+		controller : function($scope){
+			$(document).ready(function(){
+				$("#angular-side-nav-open").click(function(){
+					$(".custom-sidenav-raoit").fadeIn();
+					$(".custom-sidenav-raoit ul").animate({'width':'300px'},"slow");
 				});
-			}
+				$(".custom-sidenav-raoit").click(function(){
+					$(".custom-sidenav-raoit ul").animate({'width':'0px'},"fast","",function(){$(".custom-sidenav-raoit").fadeOut("fast")});
+				})
+			});
 		}
-	});
+	}
+});
